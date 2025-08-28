ed74e358add3f8aebf236bd3089db205fe62a1d7
sgarikapati <sgarikapati@kingsettcapital.com>
2025-08-28 16:17:15 -0400

    Environment files added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,3 +3,18 @@ export const environment = {
   production: false,
   apiKey: "118dab24-8387-40e6-865d-04f55d4aaaa9"
 };
+
+// export const environment = {
+//   apiUrl: "http://localhost:5005/api",
+//   x_api_key: "3e2aaa46-1b28-4b4c-b363-7915a4d66dc0",
+//   production: false,
+//   appVersion: require('../../package.json').version,
+//   azureConfig: {
+//     // Application/client ID of registered SPA app
+//     clientId: 'f8982f4d-6af7-4dee-ae33-6d2455bdd246',
+//     // https://login.microsoft.com/{Tenant ID of registered SPA app}
+//     authority: 'https://login.microsoftonline.com/201f8a10-22eb-42e9-ad2c-8d16feabf5ec',
+//     // Scope set for registered API app
+//     scopes: 'api://30a6d94d-df81-40ac-8cb8-af77fec0ce38/read'
+//   },
+// };
---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,19 @@
+// export const environment = {
+//     production: false,
+//     // apiUrl : 'https://ec92da980d37.ngrok.io/',
+//     apiUrl : "https://tbauatapi.kingsettcapital.com/api",
+//     x_api_key: "3e2aaa46-1b28-4b4c-b363-7915a4d66dc0",
+//     azureConfig: {
+//         // Application/client ID of registered SPA app
+//         clientId: '6873e997-79e0-4375-a975-a68399d0b270',
+//         // https://login.microsoft.com/{Tenant ID of registered SPA app}
+//         authority: 'https://login.microsoft.com/f6d94abc-5472-43af-ab66-95726e5ab0cc',
+//         // Scope set for registered API app
+//         scopes: "api://37462356-0754-4058-bc19-2d305c84e19e/Read"
+//       }
+//   };
+
+export const environment = {
+  production: false,
+  apiKey: "118dab24-8387-40e6-865d-04f55d4aaaa9"
+};
