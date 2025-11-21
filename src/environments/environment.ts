d411e27ea498604d90979e9b5e37993d8d092d3b
Lokesh Bansal <lokesh@75way.com>
2025-11-21 17:23:13 +0530

    updated environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,16 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiKey: "118dab24-8387-40e6-865d-04f55d4aaaa9"
+  apiKey: "118dab24-8387-40e6-865d-04f55d4aaaa9",
+  apiUrl: 'https://prodagatewaydev.kingsettcapital.com/api',
+  appName: 'KS DSP SPA',
+  version: '1.0.0',
+  azureConfig: {
+    clientId: '1ca558e3-5dea-49ba-8080-f490ff880c00',
+    authority: 'https://login.microsoftonline.com/f6d94abc-5472-43af-ab66-95726e5ab0cc',
+    redirectURL: 'http://localhost:4200',
+    scopes: "api://7faad414-e5b5-46f8-b2fa-9eb7272f8fea",
+  }
 };
 
 // export const environment = {
