237b41fddb8052ec05f01c2096c5fbc57892a4e0
Suresh Kannan <SKannan@Kingsettcapital.com>
2026-06-12 09:48:41 -0400

    Dev Environment variable changed

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    apiUrl: 'https://localhost:7030/api',
+    apiUrl: 'https://prodagatewaydev.kingsettcapital.com/api',
   // apiUrl : "http://localhost:5005/api",
   // apiUrl: "https://prodagatewaydev.kingsettcapital.com/api",
-  validationApiUrl: "https://localhost:7030/api",
+  validationApiUrl: "https://prodagatewaydev.kingsettcapital.com/api",
   apiKey: "118dab24-8387-40e6-865d-04f55d4aaaa9",
   production: false,
   appVersion: require('../../package.json').version,
